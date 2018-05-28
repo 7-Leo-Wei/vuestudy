b3cd107b8a3d1cdf2dba6d6f833ef2dff629fdd2
Leo <1359610593@qq.com>
2018-05-28 18:10:11 +0800

    chunjin vue study

---
+++ js/components/cpntDemo.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>这个是标题。</h1>
+	<p>这个是内容。</p>
+</template>
+
+<script>
+	export default {
+		name: 'cpntDm'
+	}
+</script>
---
+++ js/demoCpnt.js
@@ -0,0 +1,11 @@
+
+import Vue from 'vue'
+import cpntDm from './components/cpntDemo'
+
+Vue.config.productionTip = false
+
+new Vue({
+	el:'.cpntDemo_2',
+	components:{ cpntDm },
+	template: '<cpntdm />'
+})
